aee7fa0a466e3130b6fc734614a826553c932e8b
Мисак Максим <maximmisak@gmail.com>
2021-11-08 21:33:56 +0300

    final commit

---
+++ Task007/Program.cs
@@ -0,0 +1,14 @@
+﻿// Даны четыре логические значения f1, f2, f3, f4. Записать условие того, что
+// - более трёх значений истинны
+// - хотя бы три значения истинны
+// - менее трёх значений истинны
+
+bool F1 = true, F2 = true, F3 = true, F4 = true;
+int count = 0;
+if(F1 == true) count++;
+if(F2 == true) count++;
+if(F3 == true) count++;
+if(F4 == true) count++;
+if (count > 3) Console.WriteLine("более трёх значений истинны");
+if (count >= 3) Console.WriteLine("хотя бы три значения истинны");
+if (count < 3) Console.WriteLine("менее трёх значений истинны");
