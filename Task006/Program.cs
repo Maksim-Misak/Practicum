7cd3330e5d5e89a686c73506e53ea1c0c198254e
Мисак Максим <maximmisak@gmail.com>
2021-11-12 14:37:33 +0300

    Corrected logic 1,2

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -5,24 +5,22 @@
 
 
 // - целой степенью числа 3
-double m = 625;
+double m = 25;
 // double m = new Random().Next(1, 1000);
 Console.WriteLine($"m = {m}");
 double a = 3;
 double j = Math.Log(m, a);
-if (Math.Ceiling(j) - j < 0.0001) j = Math.Ceiling(j);
-if (Math.Pow(a, j) == m) Console.WriteLine($"Число {m} является степенью числа 3");
+if (Math.Truncate(j) - j == 0) Console.WriteLine($"Число {m} Является целой степенью числа 3");
 else Console.WriteLine($"Число {m} НЕ является степенью числа 3");
 
 // - корнем целой степени из 5, 
-
 double b = 5;
-double k = Math.Log(m, b);
-if (k - Math.Floor(k) < 0.0001) k = Math.Floor(k);
-if (Math.Pow(b, k) == m) Console.WriteLine($"Число {m} является корнем целой степени из 5");
+double k = Math.Log(Math.Pow(m, 2), b);
+Console.WriteLine(k);
+// if (k - Math.Floor(k) < 0.0001) k = Math.Floor(k);
+if (Math.Truncate(k) - k == 0) Console.WriteLine($"Число {m} Является корнем целой степени из 5");
 else Console.WriteLine($"Число {m} НЕ является корнем целой степени из 5");
 
 // - синусом угла кратного 45 градусам
-
-if(m == Math.Sin(45) || m == Math.Sin(90) || m == Math.Sin(135) || m == Math.Sin(180)) Console.WriteLine($"Число {m} является синусом угла кратного 45 градусам");
+if(m == Math.Sin(45 * Math.PI / 180) || m == Math.Sin(90 * Math.PI / 180) || m == Math.Sin(135 * Math.PI / 180) || m == Math.Sin(180 * Math.PI / 180)) Console.WriteLine($"Число {m} Является синусом угла кратного 45 градусам");
 else Console.WriteLine($"Число {m} НЕ является синусом угла кратного 45 градусам");
